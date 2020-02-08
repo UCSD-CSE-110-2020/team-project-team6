3638546cc53b771566cd45535b5d0b682c7aa793
coraxyc <ycxing99@gmail.com>
2020-02-08 03:11:45 -0800

    Add endpoint class for counting and updating steps from lab4

--- app/src/main/java/com/example/team_project_team6/StepCountActivity.java
+++ app/src/main/java/com/example/team_project_team6/StepCountActivity.java
@@ -1,18 +1,13 @@
-package edu.ucsd.cse110.fitnessapp;
+package com.example.team_project_team6;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import edu.ucsd.cse110.fitnessapp.fitness.FitnessService;
-import edu.ucsd.cse110.fitnessapp.fitness.FitnessServiceFactory;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.team_project_team6.fitness.FitnessService;
+import com.example.team_project_team6.fitness.FitnessServiceFactory;
 
 public class StepCountActivity extends AppCompatActivity {
 
@@ -20,29 +15,11 @@ public class StepCountActivity extends AppCompatActivity {
 
     private static final String TAG = "StepCountActivity";
 
-    private TextView textSteps;
     private FitnessService fitnessService;
-    private boolean encouragementShown = false;
-    private long stepCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_step_count);
-        textSteps = findViewById(R.id.textSteps);
-
-        String fitnessServiceKey = getIntent().getStringExtra(FITNESS_SERVICE_KEY);
-        fitnessService = FitnessServiceFactory.create(fitnessServiceKey, this);
-
-        Button btnUpdateSteps = findViewById(R.id.buttonUpdateSteps);
-        btnUpdateSteps.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fitnessService.updateStepCount();
-            }
-        });
-
-        fitnessService.setup();
 
     }
 
@@ -61,21 +38,6 @@ public class StepCountActivity extends AppCompatActivity {
     }
 
     public void setStepCount(long stepCount) {
-        textSteps.setText(String.valueOf(stepCount));
-        if(!encouragementShown && stepCount >= 1000) {
-            showEncouragement(stepCount);
-            encouragementShown = true;
-        }
-    }
 
-    public void showEncouragement(long numSteps) {
-        Context context = getApplicationContext();
-        int recNumSteps = 100;
-
-        CharSequence text = "Good job! You're already at " + numSteps/recNumSteps + "% of the daily recommended number of steps.";
-        int duration = Toast.LENGTH_LONG;
-        Toast toast = Toast.makeText(context, text, duration);
-
-        toast.show();
     }
 }
