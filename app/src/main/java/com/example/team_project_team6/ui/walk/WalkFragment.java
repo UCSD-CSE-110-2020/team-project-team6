e5bba01123feb170e93b1f4680364c2ce404361f
lethevu <kevinlethevu@gmail.com>
2020-02-08 16:18:58 -0800

    update

--- app/src/main/java/com/example/team_project_team6/ui/walk/WalkFragment.java
+++ app/src/main/java/com/example/team_project_team6/ui/walk/WalkFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -17,9 +18,13 @@ import com.example.team_project_team6.R;
 public class WalkFragment extends Fragment {
 
     private WalkViewModel dashboardViewModel;
+    private boolean switchButton = false;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+
+
         dashboardViewModel =
                 ViewModelProviders.of(this).get(WalkViewModel.class);
         View root = inflater.inflate(R.layout.fragment_walk, container, false);
@@ -30,6 +35,35 @@ public class WalkFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        final TextView lbStopWatch = root.findViewById(R.id.lbTime);
+        final Button btStart = root.findViewById(R.id.btStart);
+
+        btStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!switchButton) {
+                    dashboardViewModel.runStopWatch();
+                    //switch to stop button when it's true
+                    switchButton = true;
+                    btStart.setText("STOP");
+                }else {
+                    switchButton = false;
+                    btStart.setText("START");
+                    dashboardViewModel.stopWatch();
+                    dashboardViewModel.resetWatch();
+                }
+            }
+        });
+
+
+        dashboardViewModel.getStopWatch().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                lbStopWatch.setText(s);
+            }
+        });
+
         return root;
     }
 }
