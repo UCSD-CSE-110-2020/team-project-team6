f0bdaec9fc5fac337233ab597b4c24669cd699b1
Maryam Rahdaran <mrahdara@ucsd.edu>
2020-02-11 17:49:49 -0800

    added class for saving data

---
+++ app/src/main/java/com/example/team_project_team6/SaveDataActivity.java
@@ -0,0 +1,37 @@
+package com.example.team_project_team6;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.team_project_team6.model.Walk;
+import com.google.gson.Gson;
+
+public class SaveDataActivity extends AppCompatActivity {
+    Walk walk;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_walk);
+    }
+
+    public void startNewWalk(){
+        walk = new Walk();
+    }
+
+    public void stopWalk(String duration, long numSteps, double distance)
+    {
+        walk.setDist(distance);
+        walk.setDuration(duration);
+        walk.setStep(numSteps);
+
+//        Gson gson = new Gson();
+//        String json = gson.toJson(walk);
+//
+//        SharedPreferences spfs = getSharedPreferences("user_data", MODE_PRIVATE);
+//        SharedPreferences.Editor editor = spfs.edit();
+    }
+
+}
--- app/src/main/java/com/example/team_project_team6/ui/walk/WalkFragment.java
+++ app/src/main/java/com/example/team_project_team6/ui/walk/WalkFragment.java
@@ -34,6 +34,8 @@ public class WalkFragment extends Fragment {
         final TextView textView = root.findViewById(R.id.text_walk);
         final TextView lbStopWatch = root.findViewById(R.id.lbTime);
         final Button btStart = root.findViewById(R.id.btStart);
+        final TextView walkSteps = root.findViewById(R.id.lbStep);
+        final TextView walkDist = root.findViewById(R.id.lbDistance);
 
         final MainActivity mainActivity = (MainActivity) getActivity();
 
@@ -63,8 +65,6 @@ public class WalkFragment extends Fragment {
             }
         });
 
-        final TextView walkSteps = root.findViewById(R.id.lbStep);
-        final TextView walkDist = root.findViewById(R.id.lbDistance);
         SharedPreferences spfs = this.requireActivity().getSharedPreferences("user_data", Context.MODE_PRIVATE);
         final int heightInInches = spfs.getInt("user_height", -1);
         final double strideDistInFt = (0.413 * (double) heightInInches) / 12.0;
