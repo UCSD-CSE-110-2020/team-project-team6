423894245f027e67ffce2c77d962c60fccbcae2d
Warren Hu <me@warrenhu.com>
2020-02-15 23:34:04 -0800

    fix merge errors (again)

--- app/src/main/java/com/example/team_project_team6/ui/route_details/RouteDetailsFragment.java
+++ app/src/main/java/com/example/team_project_team6/ui/route_details/RouteDetailsFragment.java
@@ -156,7 +156,6 @@ public class RouteDetailsFragment extends Fragment {
                 if (controller.getCurrentDestination().getId() == R.id.routeDetailsFragment) {
                     controller.navigate(R.id.action_routeDetailsFragment_to_navigation_walk);
 
-
                     mViewModel.setIsWalkFromRouteDetails(true);
                     mViewModel.setRoute(route);
                 }
