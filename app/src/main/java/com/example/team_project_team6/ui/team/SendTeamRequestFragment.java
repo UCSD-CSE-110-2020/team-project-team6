3ec4b7493d79fbd4c4a9fb6d9c1427e1b090e378
Maryam Rahdaran <mrahdara@ucsd.edu>
2020-02-28 15:40:45 -0800

    add and invite button is working

--- app/src/main/java/com/example/team_project_team6/ui/team/SendTeamRequestFragment.java
+++ app/src/main/java/com/example/team_project_team6/ui/team/SendTeamRequestFragment.java
@@ -3,12 +3,17 @@ package com.example.team_project_team6.ui.team;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.team_project_team6.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 /**
@@ -61,6 +66,22 @@ public class SendTeamRequestFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_send_team_request, container, false);
+        View root = inflater.inflate(R.layout.fragment_send_team_request, container, false);
+
+        final Button btSendInvite = root.findViewById(R.id.bt_invite);
+
+        // navigate to team_fragment when send invite button is pressed
+        btSendInvite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("go back to team", "Clicked on 'send invite' button");
+                NavController controller = NavHostFragment.findNavController(requireParentFragment());
+                if (controller.getCurrentDestination().getId() == R.id.sendTeamRequestFragment) {
+                    controller.navigate(R.id.R_id_SendTeamRequestFragment_to_action_teamFragment_);
+                }
+            }
+        });
+
+        return root;
     }
 }
