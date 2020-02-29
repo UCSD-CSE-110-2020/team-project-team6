246e90abda6b8997ac950bfe6ec93e0ac9a15af1
coraxyc <ycxing99@gmail.com>
2020-02-29 15:26:32 -0800

    added list view

--- app/src/main/java/com/example/team_project_team6/ui/team/MemberFragment.java
+++ app/src/main/java/com/example/team_project_team6/ui/team/MemberFragment.java
@@ -3,6 +3,8 @@ package com.example.team_project_team6.ui.team;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -14,7 +16,11 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.team_project_team6.R;
+import com.example.team_project_team6.model.Route;
+import com.example.team_project_team6.ui.routes.RoutesViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 
 /**
@@ -23,56 +29,23 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
  * create an instance of this fragment.
  */
 public class MemberFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MemberFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MemberFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MemberFragment newInstance(String param1, String param2) {
-        MemberFragment fragment = new MemberFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private TeamViewModel teamViewModel;
+    private ArrayAdapter mfAdapter;
+    private String[] teamMemberArray;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        if (teamViewModel == null) {
+            teamViewModel = new ViewModelProvider(requireActivity()).get(TeamViewModel.class);
+        }
 
+        teamMemberArray = teamViewModel.getTeamMemberNameList().toArray(new String[0]);
         View root = inflater.inflate(R.layout.fragment_members, container, false);
-//        String[] array = {"a","b","c"};
-//        ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(),
-//                R.layout.fragment_members, array);
-//        ListView listView = (ListView) root.findViewById(R.id.list_team_members);
-//        listView.setAdapter(adapter);
+        mfAdapter = new ArrayAdapter<String>(getActivity(),
+                R.layout.single_item_list_view, teamMemberArray);
+        ListView listView = (ListView) root.findViewById(R.id.list_team_members);
+        listView.setAdapter(mfAdapter);
 
 
         final FloatingActionButton btNewInvite = root.findViewById(R.id.bt_invite_member);
@@ -91,6 +64,4 @@ public class MemberFragment extends Fragment {
         });
         return root;
     }
-
-
 }
