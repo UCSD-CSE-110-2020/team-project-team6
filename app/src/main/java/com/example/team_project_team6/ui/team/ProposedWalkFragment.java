99d4136fba81fa3b4c01bb641a226c5d1b28f1f2
Maryam Rahdaran <mrahdara@ucsd.edu>
2020-03-05 14:12:00 -0800

    proposed walk and set date

--- app/src/main/java/com/example/team_project_team6/ui/team/ProposedWalkFragment.java
+++ app/src/main/java/com/example/team_project_team6/ui/team/ProposedWalkFragment.java
@@ -2,25 +2,151 @@ package com.example.team_project_team6.ui.team;
 
 import android.os.Bundle;
 
+import androidx.annotation.VisibleForTesting;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.team_project_team6.R;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProposedWalkFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProposedWalkFragment extends Fragment {
+
+    @VisibleForTesting
+    static TeamViewModel teamViewModel = null;
+
+    private ArrayAdapter mfAdapter;
+    private String[] goingStausArray;
+    private Button bt_acceptWalk;
+    private Button bt_declineTime;
+    private Button bt_declineRoute;
+    private Button bt_schedule;
+    private Button bt_withdraw;
+    private TextView txt_routeName;
+    private TextView txt_steps;
+    private TextView txt_miles;
+    private TextView txt_date;
+    private TextView txt_time;
+    private TextView txt_startPoint;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_proposed_walk, container, false);
+
+        if (teamViewModel == null) {
+            teamViewModel = new ViewModelProvider(requireActivity()).get(TeamViewModel.class);
+        }
+
+        goingStausArray = teamViewModel.getMemberGoingStatus().toArray(new String[0]);
+        View root = inflater.inflate(R.layout.fragment_proposed_walk, container, false);
+        mfAdapter = new ArrayAdapter<String>(getActivity(),
+                R.layout.single_item_list_view,goingStausArray);
+        ListView listView = (ListView) root.findViewById(R.id.list_is_going);
+        listView.setAdapter(mfAdapter);
+
+        bt_acceptWalk = root.findViewById(R.id.bt_acceptWalk);
+        bt_declineRoute = root.findViewById(R.id.bt_declineRoute);
+        bt_declineTime = root.findViewById(R.id.bt_declineTime);
+        bt_schedule = root.findViewById(R.id.bt_Schedule);
+        bt_withdraw = root.findViewById(R.id.bt_withdraw);
+        txt_date = root.findViewById(R.id.txt_proposed_date);
+        txt_time = root.findViewById(R.id.txt_proposed_time);
+        txt_routeName = root.findViewById(R.id.txt_proposed_route_Name);
+        txt_miles = root.findViewById(R.id.txt_proposed_mile);
+        txt_steps = root.findViewById(R.id.txt_proposed_steps);
+        txt_startPoint = root.findViewById(R.id.txt_proposed_startingPoint);
+
+        if(!teamViewModel.getHasProposedWalk()) {
+            bt_acceptWalk.setVisibility(View.INVISIBLE);
+            bt_declineRoute.setVisibility(View.INVISIBLE);
+            bt_declineTime.setVisibility(View.INVISIBLE);
+            bt_schedule.setVisibility(View.INVISIBLE);
+            bt_withdraw.setVisibility(View.INVISIBLE);
+            txt_date.setVisibility(View.INVISIBLE);
+            txt_time.setVisibility(View.INVISIBLE);
+            txt_routeName.setVisibility(View.INVISIBLE);
+            txt_miles.setVisibility(View.INVISIBLE);
+            txt_steps.setVisibility(View.INVISIBLE);
+            txt_startPoint.setVisibility(View.INVISIBLE);
+        }
+        else if(teamViewModel.isMyProposedWalk()) {
+            bt_acceptWalk.setVisibility(View.INVISIBLE);
+            bt_declineRoute.setVisibility(View.INVISIBLE);
+            bt_declineTime.setVisibility(View.INVISIBLE);
+        }
+
+        else {
+            bt_schedule.setVisibility(View.INVISIBLE);
+            bt_withdraw.setVisibility(View.INVISIBLE);
+        }
+        ////button??????????????????????
+            bt_schedule.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.i("Proposed Walk fragment", "schedule walk");
+                    bt_schedule.setVisibility(View.INVISIBLE);
+                    bt_withdraw.setVisibility(View.INVISIBLE);
+                    bt_acceptWalk.setVisibility(View.INVISIBLE);
+                    bt_declineRoute.setVisibility(View.INVISIBLE);
+                    bt_declineTime.setVisibility(View.INVISIBLE);
+                }
+            });
+
+        bt_withdraw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("Proposed Walk fragment", "withdraw walk");
+                bt_schedule.setVisibility(View.INVISIBLE);
+                bt_withdraw.setVisibility(View.INVISIBLE);
+                bt_acceptWalk.setVisibility(View.INVISIBLE);
+                bt_declineRoute.setVisibility(View.INVISIBLE);
+                bt_declineTime.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        bt_acceptWalk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("Proposed Walk fragment", "accept walk");
+                bt_schedule.setVisibility(View.INVISIBLE);
+                bt_withdraw.setVisibility(View.INVISIBLE);
+                bt_acceptWalk.setVisibility(View.INVISIBLE);
+                bt_declineRoute.setVisibility(View.INVISIBLE);
+                bt_declineTime.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        bt_declineTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("Proposed Walk fragment", "decline time");
+                bt_schedule.setVisibility(View.INVISIBLE);
+                bt_withdraw.setVisibility(View.INVISIBLE);
+                bt_acceptWalk.setVisibility(View.INVISIBLE);
+                bt_declineRoute.setVisibility(View.INVISIBLE);
+                bt_declineTime.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        bt_declineRoute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("Proposed Walk fragment", "decline route");
+                bt_schedule.setVisibility(View.INVISIBLE);
+                bt_withdraw.setVisibility(View.INVISIBLE);
+                bt_acceptWalk.setVisibility(View.INVISIBLE);
+                bt_declineRoute.setVisibility(View.INVISIBLE);
+                bt_declineTime.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        return root;
     }
 }
--- app/src/main/java/com/example/team_project_team6/ui/team/TeamViewModel.java
+++ app/src/main/java/com/example/team_project_team6/ui/team/TeamViewModel.java
@@ -18,20 +18,45 @@ import androidx.lifecycle.ViewModel;
 
 public class TeamViewModel extends ViewModel {
     private MutableLiveData<ArrayList<String>> mTeamMemberNames;
+    private MutableLiveData<ArrayList<String>> memberGoingStatus;
     private boolean hasPendingTeamInvite; // records if another user has sent this user a team invite
+    private boolean hasProposedWalk; // records if user has proposedWalk
+
+    public boolean isMyProposedWalk() {
+        return isMyProposedWalk;
+    }
+
+    public void setMyProposedWalk(boolean myProposedWalk) {
+        isMyProposedWalk = myProposedWalk;
+    }
+
+    private boolean isMyProposedWalk;
+
+    public boolean getHasProposedWalk() {
+        return hasProposedWalk;
+    }
+
+    public void setHasProposedWalk(boolean hasProposedWalk) {
+        this.hasProposedWalk = hasProposedWalk;
+    }
+
+
 
     public TeamViewModel() {
         ArrayList<String> data = new ArrayList<String>();
+        ArrayList<String> goingStatus = new ArrayList<String>();
 
         // Mock names
         data.add("Perry the Platypus");
         data.add("Sarah the Soap bar");
         data.add("Ellen the Elephant");
         Collections.sort(data);
-
+//??????????????
         mTeamMemberNames = new MutableLiveData<>(data);
-
+        memberGoingStatus = new MutableLiveData<>(goingStatus);
         hasPendingTeamInvite = false;
+        hasProposedWalk = false;
+
     }
 
     public void setHasPendingTeamInvite(boolean hasPendingTeamInvite) {
@@ -42,7 +67,10 @@ public class TeamViewModel extends ViewModel {
         return hasPendingTeamInvite;
     }
 
+
     public ArrayList<String> getTeamMemberNameList() {
         return mTeamMemberNames.getValue();
+    }
+    public ArrayList<String> getMemberGoingStatus() { return memberGoingStatus.getValue();
     }
 }
